0474c28b906ec274f6732d79a5b6dedf41c8b3dc
dedave <olawoyin.david01@gmail.com>
2022-01-26 07:28:07 -0800

    app update

--- src/app/pages/tab/tab.page.ts
+++ src/app/pages/tab/tab.page.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Router} from '@angular/router';
 @Component({
   selector: 'app-tab',
   templateUrl: './tab.page.html',
@@ -7,10 +7,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TabPage implements OnInit {
 
-  constructor() { }
+  constructor(private _route: Router) { }
 
   ngOnInit() {
     
+  }
+  reset(){
+    this._route.navigate(['/tab/list'])
   }
 
 }
